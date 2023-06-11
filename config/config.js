45cda9c2de15968604a8dbe9e4ab7b7d9b03008e
sujji <psg759@gmail.com>
2023-06-11 14:59:44 +0900

    배포용 패키지 설치

--- config/config.js
+++ config/config.js
@@ -17,4 +17,26 @@ module.exports = {
         serviceId:required("NCP_SENS_ID"),
         callNumber: required("NCP_SENS_NUMBER"),
     },
+    development: {
+        username: "root",
+        password: process.env.SEQUELIZE_PASSWORD,
+        database: "nodejs",
+        host: "127.0.0.1",
+        dialect: "mysql"
+      },
+      test: {
+        username: "root",
+        password: process.env.SEQUELIZE_PASSWORD,
+        database: "database_test",
+        host: "127.0.0.1",
+        dialect: "mysql"
+      },
+      production: {
+        username: "root",
+        password: process.env.SEQUELIZE_PASSWORD,
+        database: "nodejs",
+        host: "127.0.0.1",
+        dialect: "mysql",
+        logging: false,
+      }
 };
